7ac20e69a2a40f1d62fd3659c5107a58485d0123
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2012-04-12 19:04:42 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FitBet::Application.config.secret_token = '039fbc94bd37868efd4a664c45ce254256c58bb570c87a285556018744892106ba2f3835786013460f7d246591d1b85f4b36ffadae4c5799f6ab147c1e802575'
